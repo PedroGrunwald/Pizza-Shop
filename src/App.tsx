bc3cc23a33c77ab34cfd1fe915dc53e1c2775451
Pedro Grunwald <pedro.grunwald.com>
2024-08-29 08:36:53 -0300

    chore: scheleton

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+
+import "./globals.css";
+import { router } from "./routes";
+
+export function App() {
+  return <RouterProvider router={router} />;
+}
